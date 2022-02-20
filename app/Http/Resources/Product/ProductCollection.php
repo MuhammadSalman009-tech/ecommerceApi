cb9ef0f2e45640b2b2b64b7beafb37044e683830
Muhammad Salman <salmansumra009@gmail.com>
2022-02-20 16:52:43 +0500

    created ProductCollection resource

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -14,6 +14,18 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            "data"=>$this->collection->transform(function($product){
+                return [
+                    "name"=>$product->name,
+                    "rating"=>$product->reviews->count()>0?round($product->reviews->sum("star")/$product->reviews->count(),1):"No rating yet",
+                    "totalPrice"=>round((1-($product->discount/100))*$product->price,2),
+                    "discount"=>$product->discount,
+                    "href"=>[
+                        "link"=>route("products.show",$product->id)
+                    ],
+                ];
+            })
+        ];
     }
 }
